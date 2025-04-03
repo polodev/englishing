76a62f0772d66290a2374ce86e2284b0a9d45337
polodev <polodev10@gmail.com>
2025-04-03 06:40:43 +0600

    --updated--

--- app-modules/expression/database/migrations/2025_03_31_000001_create_expressions_table.php
+++ app-modules/expression/database/migrations/2025_03_31_000001_create_expressions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('expression');
             $table->string('type');
+            $table->string('source')->nullable();
             $table->string('slug')->unique();
             $table->timestamps();
         });
