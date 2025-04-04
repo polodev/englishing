af211b1f89719a5d0295bd327f8a0887d47e07b3
polodev <polodev10@gmail.com>
2025-04-05 02:19:17 +0600

    just removed old expression

---
+++ app-modules/old-expression/src/Providers/ExpressionServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Expression\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExpressionServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
